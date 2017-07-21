d71a43e3b02dd98ee850a892a0ab825cba6cbb2d
Caio Amaral Corrêa <caiobep@me.com>
2017-07-21 13:32:51 -0300

    Basic Pages navigation structure

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,14 +1,36 @@
 import React from 'react'
 
+import { Switch, Route } from 'react-router-dom'
+
 /* Local Imports */
 import AppNav from './components/AppNav.jsx'
 import DrawerButton from './components/DrawerButton.jsx'
 
+/* Pages Imports */
+import Home from './pages/Home.jsx'
+import Funcionarios from './pages/Funcionarios.jsx'
+
+/* Icons Imports */
+import HomeIcon from 'material-ui-icons/Home'
+import PeopleIcon from 'material-ui-icons/People'
+
 const App = () => (
   <div>
-    <AppNav title='Amaral Guincho'>
-      <DrawerButton to='/' exact text='Home' />
+    <AppNav>
+      <DrawerButton to='/' text='Home'
+        icon={<HomeIcon />}
+      />
+      <DrawerButton to='/funcionarios' text='Funcionarios'
+        icon={<PeopleIcon />}
+      />
     </AppNav>
+
+    <div className='app-content'>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/funcionarios' component={Funcionarios} />
+      </Switch>
+    </div>
   </div>
 )
 

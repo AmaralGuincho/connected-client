26db5f228649b1abe49ced860bd0f4cb20c2f119
Caio Amaral Corrêa <caiobep@me.com>
2017-07-24 14:44:14 -0300

    Funcionarios api to server root

--- server/main.js
+++ server/main.js
@@ -1,5 +1,7 @@
-import { Meteor } from 'meteor/meteor';
+import { Meteor } from 'meteor/meteor'
+
+import '../imports/api/Funcionarios'
 
 Meteor.startup(() => {
   // code to run on server at startup
-});
+})
